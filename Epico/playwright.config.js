5622dd0151bd96de849844720cbb7cdf1fdd7b3d
Suraj Bhardwaj <suraj.bhardwaj@ad.mindbowser.com>
2026-06-03 17:03:13 +0530

    auth update

--- Epico/playwright.config.js
+++ Epico/playwright.config.js
@@ -57,17 +57,17 @@ export default defineConfig({
     },
 
     /*
-        {
-          name: 'firefox',
-          use: { ...devices['Desktop Firefox'] },
-        },
-    
-        {
-          name: 'webkit',
-          use: { ...devices['Desktop Safari'] },
-        },
-    
-        /* Test against mobile viewports. */
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },  */
+
+    /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
